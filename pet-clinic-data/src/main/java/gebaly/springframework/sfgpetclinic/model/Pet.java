510407aaa25d6fe93d271049963880624c880329
M-Gebaly <M-Gebaly@users.noreply.github.com>
2020-05-26 22:56:47 +0200

    Adding Base Entity. Closes #10

--- pet-clinic-data/src/main/java/gebaly/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/gebaly/springframework/sfgpetclinic/model/Pet.java
@@ -2,7 +2,7 @@ package gebaly.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity {
 
     private PetType petType;
     private Owner owner;
