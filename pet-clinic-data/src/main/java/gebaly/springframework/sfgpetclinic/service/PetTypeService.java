b32d7b8a90b3ce8cde721434c02936454512bc26
M-Gebaly <m.gebaly1993@gmail.com>
2020-08-20 19:25:01 +0200

    Create PetType Map Service Impl. Closes #29

---
+++ pet-clinic-data/src/main/java/gebaly/springframework/sfgpetclinic/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package gebaly.springframework.sfgpetclinic.service;
+
+import gebaly.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
