f6ec471c4cdcfee24d042f49f7b8794a5b4936d9
M-Gebaly <m.gebaly1993@gmail.com>
2020-08-21 02:14:09 +0200

    Create Specialty Map Service Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/gebaly/springframework/sfgpetclinic/service/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package gebaly.springframework.sfgpetclinic.service;
+
+import gebaly.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
